8a8328fc8698ae678beec6477024ea99d2ef31c4
Ramaiz Mansoor <rmansoor@pentaho.com>
2015-10-12 15:59:41 -0400

    Creating a maven project for custom authentication

---
+++ src/main/java/org/pentaho/custom/authentication/CustomUserDetailsService.java
@@ -0,0 +1,145 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2015 Pentaho Corporation..  All rights reserved.
+ */
+package org.pentaho.custom.authentication;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import org.pentaho.platform.authentication.hibernate.IRole;
+import org.pentaho.platform.authentication.hibernate.IUser;
+import org.pentaho.platform.authentication.hibernate.IUserRoleDao;
+import org.pentaho.platform.authentication.hibernate.UncategorizedUserRoleDaoException;
+import org.pentaho.platform.api.mt.ITenantedPrincipleNameResolver;
+import org.springframework.dao.DataAccessException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+
+/**
+ * A <code>UserDetailsService</code> that delegates to an {@link IUserRoleDao} to load users by username.
+ * 
+ */
+public class CustomUserDetailsService implements UserDetailsService {
+
+  // ~ Static fields/initializers ====================================================================================== 
+
+  // ~ Instance fields =================================================================================================
+
+  private IUserRoleDao userRoleDao;
+  
+  /**
+   * A default role which will be assigned to all authenticated users if set
+   */
+  private GrantedAuthority defaultRole;
+  
+  private ITenantedPrincipleNameResolver userNameUtils;
+  
+  // ~ Constructors ====================================================================================================
+
+  // ~ Methods =========================================================================================================
+
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException, DataAccessException {
+    final boolean ACCOUNT_NON_EXPIRED = true;
+    final boolean CREDS_NON_EXPIRED = true;
+    final boolean ACCOUNT_NON_LOCKED = true;
+
+    // Retrieve the user from the custom authentication system
+    IUser user;
+    try {
+      user = userRoleDao.getUser(getUserNameUtils().getPrincipleName( username));
+    } catch (UncategorizedUserRoleDaoException e) {
+      throw new UserDetailsException("Unable to get the user role dao"); //$NON-NLS-1$
+    }
+
+    // If the user is null, throw a NameNotFoundException
+    if (user == null) {
+      throw new UsernameNotFoundException("Username [ " + getUserNameUtils().getPrincipleName( username) + "] not found"); //$NON-NLS-1$
+    } else {
+      // convert IUser to a UserDetails instance
+      int authsSize = user.getRoles() != null ? user.getRoles().size() : 0;
+      GrantedAuthority[] auths = new GrantedAuthority[authsSize];
+      int i = 0;
+      for (IRole role : user.getRoles()) {
+        auths[i++] = new SimpleGrantedAuthority(role.getName());
+      }
+
+      List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>(Arrays.asList(auths));
+      
+      if (authorities.size() == 0) {
+        throw new UsernameNotFoundException("User [ " + getUserNameUtils().getPrincipleName( username) + "] does not have any role"); //$NON-NLS-1$
+      }
+      
+      // Add default role to all authenticating users
+      if ( defaultRole != null && !authorities.contains( defaultRole ) ) {
+        authorities.add( defaultRole );
+      }
+
+      GrantedAuthority[] arrayAuths = authorities.toArray(new GrantedAuthority[authorities.size()]);
+
+      return new User(user.getUsername(), user.getPassword(), user.isEnabled(), ACCOUNT_NON_EXPIRED, CREDS_NON_EXPIRED,
+          ACCOUNT_NON_LOCKED, Arrays.asList(arrayAuths));
+    }
+  }
+
+  
+  public void setUserRoleDao(IUserRoleDao userRoleDao) {
+    this.userRoleDao = userRoleDao;
+  }
+
+
+  public ITenantedPrincipleNameResolver getUserNameUtils() {
+    return userNameUtils;
+  }
+
+  public void setUserNameUtils( ITenantedPrincipleNameResolver userNameUtils ) {
+    this.userNameUtils = userNameUtils;
+  }  
+  
+  /**
+   * The default role which will be assigned to all users.
+   *
+   * @param defaultRole the role name, including any desired prefix.
+   */
+  public void setDefaultRole(String defaultRole) {
+      this.defaultRole = new SimpleGrantedAuthority(defaultRole);
+  }
+  
+  /**
+   * A data access exception specific to a <code>IUserRoleDao</code>-based <code>UserDetailsService</code>.
+   */
+  protected class UserDetailsException extends DataAccessException {
+
+    private static final long serialVersionUID = -3598806635515478946L;
+
+    public UserDetailsException(String msg) {
+      super(msg);
+    }
+
+    public UserDetailsException(String msg, Throwable cause) {
+      super(msg, cause);
+    }
+
+  }
+
+
+}
---
+++ src/main/java/org/pentaho/custom/authentication/CustomUserRoleListService.java
@@ -0,0 +1,166 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2015 Pentaho Corporation..  All rights reserved.
+ */
+package org.pentaho.custom.authentication;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import org.pentaho.platform.api.engine.IUserRoleListService;
+import org.pentaho.platform.api.mt.ITenant;
+import org.pentaho.platform.api.mt.ITenantedPrincipleNameResolver;
+import org.pentaho.platform.authentication.hibernate.CustomRole;
+import org.pentaho.platform.authentication.hibernate.IRole;
+import org.pentaho.platform.authentication.hibernate.IUser;
+import org.pentaho.platform.authentication.hibernate.IUserRoleDao;
+
+/**
+ * An {@link IUserRoleListService} that delegates to an {@link IUserRoleDao}.
+ * 
+ * 
+ */
+public class CustomUserRoleListService implements IUserRoleListService {
+
+  // ~ Static fields/initializers ====================================================================================== 
+
+  // ~ Instance fields =================================================================================================
+
+  private IUserRoleDao userRoleDao;
+  
+  private String defaultRole;
+
+  private ITenantedPrincipleNameResolver userNameUtils;
+
+  private ITenantedPrincipleNameResolver roleNameUtils;
+
+  // ~ Constructors ====================================================================================================
+
+  public CustomUserRoleListService() {
+    super();
+  }
+
+  // ~ Methods =========================================================================================================
+
+  @Override
+  public List<String> getAllRoles() {
+    return getAllRoles(null);
+  }
+
+  @Override
+  public List<String> getAllUsers() {
+    return getAllUsers(null);
+  }
+
+  @Override
+  public List<String> getSystemRoles() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public List<String> getAllRoles( ITenant tenant ) {
+    List<IRole> roles = userRoleDao.getRoles();
+
+    List<String> auths = new ArrayList<String>(roles.size());
+
+    for ( IRole role : roles ) {
+        auths.add( role.getName() );
+    }
+    
+    return auths;
+  }
+
+  @Override
+  public List<String> getAllUsers( ITenant tenant ) {
+    List<IUser> users = userRoleDao.getUsers();
+
+    List<String> usernames = new ArrayList<String>();
+
+    for (IUser user : users) {
+      usernames.add(user.getUsername());
+    }
+
+    return usernames;
+  }
+
+  @Override
+  public List<String> getUsersInRole( ITenant tenant, String roleName ) {
+
+    String updateRole = roleNameUtils.getPrincipleName( roleName );
+    IRole role = userRoleDao.getRole(updateRole);
+    if (role == null) {
+      return Collections.emptyList();
+    }
+
+    List<String> usernames = new ArrayList<String>();
+
+    for (IUser user : role.getUsers()) {
+      usernames.add(user.getUsername());
+    }
+
+    return usernames;
+
+  }
+
+  @Override
+  public List<String> getRolesForUser( ITenant tenant, String username ) {
+	IUser user = userRoleDao.getUser(username);
+	// If no user found return null
+	if ( user == null ) {
+		return null;
+	}
+
+	// Retrieve the user from the customer authentication provider
+	Set<IRole> roleSet = user.getRoles();
+	
+	// Add the default role to the list of roles retrieved from the user
+	if ( defaultRole != null && !roleSet.contains( defaultRole ) ) {
+	   	roleSet.add( new CustomRole(defaultRole) );
+	}	  
+    List<String> roles = new ArrayList<String>(roleSet.size());
+    for (IRole role : roleSet) {
+        roles.add( role.getName() );
+    }
+    return roles;
+  }
+
+  public void setUserRoleDao(IUserRoleDao userRoleDao) {
+    this.userRoleDao = userRoleDao;
+  }
+
+  public void setDefaultRole(String defaultRole) {
+      this.defaultRole = defaultRole;
+  }
+
+
+  public ITenantedPrincipleNameResolver getUserNameUtils() {
+    return userNameUtils;
+  }
+
+  public void setUserNameUtils( ITenantedPrincipleNameResolver userNameUtils ) {
+    this.userNameUtils = userNameUtils;
+  }
+
+  public ITenantedPrincipleNameResolver getRoleNameUtils() {
+    return roleNameUtils;
+  }
+
+  public void setRoleNameUtils( ITenantedPrincipleNameResolver roleNameUtils ) {
+    this.roleNameUtils = roleNameUtils;
+  }
+}
